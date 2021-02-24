57cfb6cdeeda40ddd5e0eeb809bae784e0ced515
Kseniia <57846803+KseniiaO@users.noreply.github.com>
2021-02-24 22:28:01 +0300

    Create awk_3.py

---
+++ awk_3.py
@@ -0,0 +1,15 @@
+total_data = {}
+logs = input("Please provide full path to the apaches log file: ")
+
+with open(logs, "r") as file:
+    for line in file: 
+        ip = line.split(' ')[0]
+        data_bytes = line.split(' ')[9]
+        if ip in total_data:
+            total_data[ip] += int(data_bytes)
+        else:
+            total_data[ip] = int(data_bytes)
+file.close()
+
+for i, j in total_data.items():
+    print(f'{j} bytes for {i}')            
