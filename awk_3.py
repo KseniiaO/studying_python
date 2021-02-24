599e3ec402bc3bb45cd10b99fdbc2f8cb994662d
Kseniia Obukhova <obukhova.ksenia@gmail.com>
2021-02-24 23:06:52 +0300

    added try... except

--- awk_3.py
+++ awk_3.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
+
 total_data = {}
 logs = input("Please provide full path to the apaches log file: ")
 
 with open(logs, "r") as file:
-    for line in file: 
-        ip = line.split(' ')[0]
-        data_bytes = line.split(' ')[9]
-        if ip in total_data:
-            total_data[ip] += int(data_bytes)
-        else:
-            total_data[ip] = int(data_bytes)
+    for line in file:
+        try: 
+            ip = line.split(' ')[0]
+            data_bytes = line.split(' ')[9]
+            if ip in total_data:
+                total_data[ip] += int(data_bytes)
+            else:
+                total_data[ip] = int(data_bytes)
+        except (IndexError, ValueError) as e:
+            pass
 file.close()
 
 for i, j in total_data.items():
